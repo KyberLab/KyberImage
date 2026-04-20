58efcfc90720469a023be0a58cf9154504e3b95a
LastRitter <lastritter@gmail.com>
2026-04-21 00:45:47 +0800

    fix repo init nesting error for repo fetch.
    Change-Id: I45c5d39fa2795be84be7aa854d1a46e5ad6e51d7

--- method/Repo.mk
+++ method/Repo.mk
@@ -13,5 +13,12 @@
 # $(5) fetch url
 # $(6) fetch ref
 define image_run_fetch_repo
-	$(Q)mkdir -pv $(3) && cd $(3) && $(if $(call file_is_exist,$(3)/.repo),repo init -u $(5) -m $(1) -b $(6) &&) repo sync -j$$(nproc)
+	$(Q)$(if $(call file_is_exist,$(WORKSPACE_ROOT_PATH)/.repo),,mv $(WORKSPACE_ROOT_PATH)/.repo{,.temp})
+	$(Q)mkdir -pv $(3) && cd $(3) && \
+		export GIT_COMMITTER_NAME="temp" && \
+		export GIT_COMMITTER_EMAIL="temp@example.com" && \
+		export GIT_AUTHOR_NAME="temp" && \
+		export GIT_AUTHOR_EMAIL="temp@example.com" && \
+		$(if $(call file_is_exist,$(3)/.repo),repo init -u $(5) -m $(1) -b $(6) &&) repo sync -j$$(nproc)
+	$(Q)$(if $(call file_is_exist,$(WORKSPACE_ROOT_PATH)/.repo.temp),,mv $(WORKSPACE_ROOT_PATH)/.repo{.temp,})
 endef
